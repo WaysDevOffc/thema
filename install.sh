e2866ba8a9811b19e8d432b6cdd72b328f09b568
WaysDevOfficial <waysdevofficial@gmail.com>
2024-08-26 13:15:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ check_token() {
     echo -e "${GREEN}Buy dulu Gih Ke WaysDevOfficial${NC}"
     echo -e "${YELLOW}TELEGRAM : https://t.me/userkingss${NC}"
     echo -e "${YELLOW}WHATSAPP : 628568773831${NC}"
-    echo -e "${YELLOW}HARGA TOKEN : 10K FREE UPDATE JIKA ADA TOKEN BARU${NC}"
+    echo -e "${YELLOW}HARGA TOKEN : 25K FREE UPDATE JIKA ADA TOKEN BARU${NC}"
     echo -e "${YELLOW}©WaysDevOfficial${NC}"
     exit 1
   fi
