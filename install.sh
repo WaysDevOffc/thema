e9aaaf1fca06a44aebb07a16ade5f72b0cb423d4
WaysDevOfficial <waysdevofficial@gmail.com>
2024-08-26 13:17:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "12345" ]; then
+  if [ "$USER_TOKEN" = "WaysGanteng" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke WaysDevOfficial${NC}"
